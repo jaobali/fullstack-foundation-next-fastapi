ea5e4920716ae09017851f2c2a8615dec192f9fa
João Balieiro <157887597+jaobali@users.noreply.github.com>
2026-07-26 19:36:51 +0000

    trocando end poitn do status

--- frontend/tests/integration/get.test.js
+++ frontend/tests/integration/get.test.js
@@ -1,5 +1,5 @@
 test("sdgfnsdofgiosfg", async () => {
-  const response = await fetch("http://localhost:8000/status")
+  const response = await fetch("http://localhost:8000/database_status")
 
   if (!response.ok) {
     throw new Error("Erro ao consultar status do banco")
